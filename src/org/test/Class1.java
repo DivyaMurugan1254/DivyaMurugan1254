08d18ced7b7cfaa0aa32fc20ab3b7ee971a55183
DivyaMurugan1254 <divyamurugan1254@gmail.com>
2019-09-12 13:08:20 +0530

    new lines added

--- src/org/test/Class1.java
+++ src/org/test/Class1.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Class1 {
 	public static void main(String[] args) {
-     System.setProperty("webdriver.chrome.driver","C:\\Users\\Home-PC\\eclipse-workspace\\JavaProject1\\drivers\\chromedriver.exe");
+     System.setProperty("webdriver.chrome.driver","C:\\Users\\Home-PC\\eclipse-workspace\\DivyaMurugan1254\\drivers\\chromedriver.exe");
      WebDriver driver = new ChromeDriver();
      driver.get("https://www.facebook.com/");
-     
+     String title = driver.getTitle();
+     String currentUrl = driver.getCurrentUrl();
+     System.out.println(title);
+     System.out.println(currentUrl);
+     driver.quit();
 }
 }
